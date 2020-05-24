d29383edab9b6337f0e12a75f18c31032bb1b566
ronething <axingfly@gmail.com>
2020-05-24 14:43:43 +0800

    feat: template func

--- tornado_overview/sample02/template.py
+++ tornado_overview/sample02/template.py
@@ -9,16 +9,45 @@ from tornado import web, ioloop
 
 
 class MainHandler(web.RequestHandler):
+    def calc_total(self, price, nums):
+        return price * nums
+
     async def get(self, *args, **kwargs):
         word = "hello panda"
         # t = template.Template("<h3>{{ word }}</h3>")
         # loader = template.Loader("/Users/ronething/PycharmProjects/python-async/tornado_overview/sample02/templates")
         # self.finish(loader.load("hello.html").generate(word=word))
-        self.render("hello.html", word=word)
+        # self.render("hello.html", word=word)
         # self.render_string()
+        orders = [
+            {
+                "name": "小米 T 恤 忍者米兔双截棍 军绿 XXL",
+                "image": "http://i1.mifile.cn/a1/T11lLgB5YT1RXrhCrK!40x40.jpg",
+                "price": 39,
+                "nums": 3
+            },
+
+            {
+                "name": "招财猫米兔 白色",
+                "image": "http://i1.mifile.cn/a1/T14BLvBKJT1RXrhCrK!40x40.jpg",
+                "price": 49,
+                "nums": 2
+            },
+
+            {
+                "name": "小米圆领纯色 T 恤 男款 红色 XXL",
+                "image": "http://i1.mifile.cn/a1/T1rrDgB4DT1RXrhCrK!40x40.jpg",
+                "price": 59,
+                "nums": 1
+            }
+        ]
+        # self.render("index.html", orders=orders, calc_total=self.calc_total)
+        self.render("index.html", orders=orders)
 
 
 settings = {
+    "static_path": "/Users/ronething/PycharmProjects/python-async/tornado_overview/sample02/static",
+    "static_url_prefix": "/static/",
     "template_path": "templates",
 }
 
---
+++ tornado_overview/sample02/utils.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 _*-
+"""
+    created by ashing 2020/5/24 2:41 下午
+    Less is more.
+"""
+
+
+def calc_total(price, nums):
+    return price * nums
